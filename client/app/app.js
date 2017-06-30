c257fbd837701d9a0d19ed156731b96b4856eb10
rootbean <ruber19@gmail.com>
2017-06-30 14:58:28 -0500

    Se agrega funcionalidad para subir imagen de perfil en tipo blob

--- client/app/app.js
+++ client/app/app.js
@@ -1,21 +1,23 @@
 'use strict';
 
 angular.module('videoClubApp', [
-    'videoClubApp.constants', 
-    'ngCookies', 
-    'ngResource', 
-    'ngMessages', 
-    'ngSanitize',
-    'ui.router',
-    'ngFileUpload', 
-    'satellizer'
-  ])
-  .constant("API","http://localhost:25956/adsi2017_backend_completo")
-  .config(function($urlRouterProvider, $locationProvider,$authProvider,API) {
-    $urlRouterProvider.otherwise('/');
-    $locationProvider.html5Mode(true);
-    // Config Satellizer
-      $authProvider.loginUrl = API+'/api/auth/login';
-      $authProvider.tokenName = 'token';
-      $authProvider.tokenPrefix = 'VideoClubAdsi';
-  });
+        'videoClubApp.constants',
+        'ngCookies',
+        'ngResource',
+        'ngMessages',
+        'ngSanitize',
+        'ui.router',
+        'ngFileUpload',
+        'satellizer',
+        'ngImgCrop',
+        'ngFileSaver'
+    ])
+    .constant("API", "http://localhost:8080/adsi2017_backend_completo")
+    .config(function($urlRouterProvider, $locationProvider, $authProvider, API) {
+        $urlRouterProvider.otherwise('/');
+        $locationProvider.html5Mode(true);
+        // Config Satellizer
+        $authProvider.loginUrl = API + '/api/auth/login';
+        $authProvider.tokenName = 'token';
+        $authProvider.tokenPrefix = 'VideoClubAdsi';
+    });
